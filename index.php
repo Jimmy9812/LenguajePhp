ca0e6bd310fa120c2236e7ce27b1e51f51a064c1
Jimmy9812 <wladimirmaila1998pm@gmail.com>
2024-11-11 09:28:12 -0500

    feat: :sparkles: create project in php language

---
+++ index.php
@@ -0,0 +1,13 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP with Apache and Docker</title>
+</head>
+<body>
+    <h1><?php echo "Hello, PHP with Apache and Docker!"; ?></h1>
+    <p>This is a simple PHP application served by Apache within a Docker container.</p>
+</body>
+</html>
